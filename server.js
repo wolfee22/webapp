a8ed42f4625bfdbc8e81b98bcc34372b48abcf96
10student <10student@nr.stu>
2018-02-15 20:40:12 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var connect  = require('connect');
+var static = require('serve-static');
+
+var server = connect();
+
+server.use(  static(__dirname + '/public'));
+
+server.listen(3000);
+
+console.log("Server is running. Press CTRL + C to exit.")
+
+var livereload = require('livereload');
+var lrserver = livereload.createServer();
+lrserver.watch(__dirname + "/public");
